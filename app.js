59169834de467349bfaba0da633357db371ae822
Pedro Bala Simón <pbalasimon@gmail.com>
2020-12-23 12:53:17 +0100

    refactor: Refactor Dino and human class

--- app.js
+++ app.js
@@ -1,58 +1,84 @@
+let dinos = [];
 
-    // Create Dino Constructor
-    class Dino {
-        constructor(species, weight, height, diet, where, when, fact) {
-            this.species = species;
-            this.weight = weight;
-            this.height = height;
-            this.diet = diet;
-            this.where = where;
-            this.when = when;
-            this.fact = fact;
-        }
-
+// Create Animal class
+class Animal {
+    constructor(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
     }
+}
 
-
-    // Create Dino Objects
-    const createDinoObjects = async ()=> {
-        try {
-            const dinos = await fetch("dino.json");
-            console.log(await dinos.json());
-        } catch (error) {
-            console.error(error);
-        }
+// Create Human class
+class Human extends Animal {
+    constructor(name, feet, inches, weight, diet) {
+        super("human", weight, null, diet, null, null, null);
+        this.name = name;
+        this.feet = feet;
+        this.inches = inches;
     }
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
+}
 
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Objects
+const getDinos = async () => {
+    try {
+        const data = await fetch("dino.json");
+        const json = await data.json();
+        const dinos = [];
+        json.dinos.map(dino => {
+            const { species, weight, height, diet, where, when, fact } = dino;
+            dinos.push(new Animal(species, weight, height, diet, where, when, fact));
+        });
+        return dinos;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Create Human Object
+const handleCompare = async () => {
+
+    // FIXME Validate form
+    const name = document.querySelector("#name").value;
+    const feet = document.querySelector("#feet").value;
+    const inches = document.querySelector("#inches").value;
+    const weight = document.querySelector("#weight").value;
+    const diet = document.querySelector("#diet").value;
+    const human = new Human(name, feet, inches, weight, diet);
+    console.log(human);
+    dinos = await getDinos();
+    console.log(dinos);
+}
 
 
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Use IIFE to get human data from form
 
 
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
 
-    // Generate Tiles for each Dino in Array
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-        // Add tiles to DOM
 
-    // Remove form from screen
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
-(()=>{
-    console.log('DOM Loaded!');
-    createDinoObjects();
-    document.querySelector("#compare").addEventListener('click', ()=> {
-        console.log('Comparing...');
-    })
+(() => {
+    document.querySelector("#compare").addEventListener('click', handleCompare);
 })();
